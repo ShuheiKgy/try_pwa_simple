ad5fa64809da3588a166749ba554f31634fffbff
Shuhei Kagaya <shuhei.lab@gmail.com>
2019-05-04 00:20:37 +0900

    workboxに対応

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const { GenerateSW } = require("workbox-webpack-plugin");
+
+module.exports = {
+    plugins: [
+        new GenerateSW({
+            swDest: "sw.js",
+            skipWaiting: true,
+            clientsClaim: true,
+            navigateFallback: "/index.html"
+        })
+    ]
+};
